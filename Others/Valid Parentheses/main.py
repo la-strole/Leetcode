f72c47e50e16d21fd385532875c3de353afeb374
la-strole <la-strole.sovaleto48@gmail.com>
2022-06-13 18:05:55 +0300

    Valid parantheses:

--- Others/Valid Parentheses/main.py
+++ Others/Valid Parentheses/main.py
@@ -1,21 +1,19 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        d = {')': '(', '[': ']', '{':'}'}
-        close_index = 0
-        close_flag = 0
+        d = {')': '(', ']': '[', '}': '{'}
+        stack = []
         for letter in s:
-            if close_flag == 0 and letter not in d.values():
-                return False
-            elif letter in d.keys():
-                close_flag = 1
-                while close_index >= 0:
-                    if s[close_index - 1] != d[s[close_index]]:
-                        return False
+            if letter in d:
+                if not stack or stack.pop() != d[letter]:
+                    return False
 
-
-            close_index += 1
-
+            else:
+                stack.append(letter)
+        if not stack:
+            return True
+        return False
 
 
 if __name__ == "__main__":
-    s =
+    s = "(]"
+    print(Solution().isValid(s))
